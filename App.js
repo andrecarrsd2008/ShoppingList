89fc5757999679f9f3de31e30877a71ee977603e
andrecarrsd2008 <andrecarrsd2008>
2020-02-20 17:13:40 -0800

    added AddItem file

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import {uuidv4, uuid} from 'uuidv4';
 import Header from './components/Header';
 import ListItem from './components/ListItem.js';
-import {uuidv4, uuid} from 'uuidv4';
+import AddItem from './components/AddItem.js';
+
+
 const App = () => {
   const [items, setItems] = useState([
     {id: uuid(), text: 'Milk'},
@@ -11,12 +14,31 @@ const App = () => {
     {id: uuid(), text: 'Juice'},
   ]);
 
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = (text) => {
+    if(!text) {
+    Alert.alert('Error', 'Please enter an item', { text: 'ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+   };
+
   return (
    <View style={styles.container}>
      <Header />
+     <AddItem addItem={addItem}/>
      <FlatList 
      data={items} 
-     renderItem={({item}) => <ListItem item={item} />}
+     renderItem={({item}) => (
+     <ListItem item={item} deleteItem={deleteItem}/>
+     )}
      />
    </View>
   );
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dis/FontAwesome';
 
-const ListItem = ({title}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
    <TouchableOpacity style={styles.listItem}>
      <View style={styles.listItemView}>
-         
+  <Text style={styles.listItemText}>{item.text}</Text>
+     <Icon 
+     name="remove" 
+     size={20} 
+     color="firebrick" 
+     onPress={() => deleteItem(item.id)}
+     />
      </View>
    </TouchableOpacity>
   );
@@ -13,7 +20,20 @@ const ListItem = ({title}) => {
 
 
 const sytles = StyleSheet.create({
-  
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    }, 
+  listItemText: {
+    fontSize: 18,
+  }
 });
 
 export default ListItem;
